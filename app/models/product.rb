f2bd7eb503c72ea873cae6fd9556bc201641bdbb
huzefahuzu <huzefabiyawarwala045@gmail.com>
2015-11-06 16:27:02 +0530

    Added Validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,12 @@
 class Product < ActiveRecord::Base
+
+	validates :title , :description , :image_url , presence: true
+	validates :price , numericality: { greater_than_or_equal_to: 0.01 }
+	validates :title , uniqueness: true
+
+	validates :image_url, :format => {
+    :with    => %r{\.(gif|jpg|png)\z}i,       # Here the original version of the book won't work which is %r{\.(gif|jpg|png)$}i,
+    										  # the reason is ^ and $ are line anchors and \z is a string anchor which is what we want instead of line anchor.
+    :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,60 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+	fixtures :products
+ 
+	test "product attributes must not be empty" do  
+		
+		product = Product.new
+
+		assert product.invalid?
+		assert product.errors[:title].any? 
+		assert product.errors[:description].any?
+		assert product.errors[:price].any?
+		assert product.errors[:image_url].any?
+
+	end
+
+
+	test "product price must be positive" do
+ 			
+		product = Product.new( title: "mu book" ,
+							   description: "ssss" ,
+							   image_url: "dsd.jpg")
+
+		product.price = -1
+		assert product.invalid?
+
+		assert_equal "must be equal to or greater than 0.01", 
+			product.errors[:price].join('; ')
+
+
+			product.price = 0
+			assert product.invalid?
+			assert_equal "must be greater than or equal to 0.01",
+			product.errors[:price].join('; ')
+
+			product.price = 1 
+			assert product.valid?
+
+
+	end
+
+	test "product is not valid without a unique title ." do
+
+			product = Product.new( title: products(:ruby).title,
+								   description: "abc" ,
+								   price: 3 , 
+								   image_url: "dw.jpg" )
+
+			assert !product.save
+			assert_equal "has already been taken" , product.errors[:title].join('; ')
+
+	end
+
+
+
+
+
 end
